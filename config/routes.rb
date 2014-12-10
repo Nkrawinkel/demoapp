093e3205cc83079c4d8731df60e01881a1883fcc
Nikolas Krawinkel <nkrawinkel.mba2015@london.edu>
2014-12-10 01:01:37 +0000

    Created users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
+
+  resources :tweets
+
   root 'static_pages#home'
 
   match '/help', to: 'static_pages#help', via: 'get'
-
-  resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
